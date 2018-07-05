5197f408ee36e66b8ad5b416244a2dd2ce3f45fc
liuchunliang <Chunliang.Liu@abcam.com>
2018-07-05 13:56:40 +0800

    前端功能完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,9 @@
 //= require jquery.dataTables
 
 // 这几个是图形
-//= require jquery.flot
-//= require jquery.flot.pie
-//= require jquery.flot.resize
+//= require flot/jquery.flot
+//= require flot/jquery.flot.pie
+//= require flot/jquery.flot.resize
 
 //= require common
 
