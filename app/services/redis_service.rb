8e9df0b547adb558f3c40f5f5f388d2ecc5ada18
liuchunliang <Chunliang.Liu@abcam.com>
2018-07-10 18:44:46 +0800

    完成filecache类

--- app/services/redis_service.rb
+++ app/services/redis_service.rb
@@ -1,7 +1,15 @@
 class RedisService
   # 初始化redis链接
   def initialize
-    @redis = Redis.new(host: "127.0.0.1", port: 6379)
+
+    begin
+      @redis = Redis.new(host: "127.0.0.1", port: 6379)
+    rescue Redis::CannotConnectError
+      puts "链接错误"
+    ensure
+      @redis = FileCache.new
+    end
+
   end
 
   def setJsTicket(ticket)
@@ -9,7 +17,7 @@ class RedisService
   end
 
   def getJsTicket()
-    return @redis.get("js_ticket");
+    return @redis.get("js_ticket")
   end
 
   def setCorpAccessToken(accessToken)
@@ -32,123 +40,56 @@ class RedisService
 end
 
 
+class FileCache
 
-# begin
-#   @redis.get(name)
-# rescue Redis::CannotConnectError
-#   nil
-# end
+  def set(key,value,options = {})
+    ex = options[:ex] ? Time.now.to_i + options[:ex] : 0
+    if key && value
+      data = get_file(Rails.root.to_s + "/config/filecache.yml")
 
+      item = {}
+      item["#{key}"] = value
+      item['expire_time'] = ex
+      item['create_time'] = Time.now.to_i
+      data["#{key}"] = item
+      set_file(Rails.root.to_s + "/config/filecache.yml",data.to_json)
+    end
+  end
 
-#
-# <?php
-#
-# class Cache
-# {
-#     public function setJsTicket($ticket)
-#     {
-#         $memcache = $this->getMemcache();
-#         $memcache->set("js_ticket", $ticket, time() + 7000); // js ticket有效期为7200秒，这里设置为7000秒
-#     }
-#
-#     public function getJsTicket()
-#     {
-#         $memcache = $this->getMemcache();
-#         return $memcache->get("js_ticket");
-#     }
-#
-#     public function setCorpAccessToken($accessToken)
-#     {
-#         $memcache = $this->getMemcache();
-#         $memcache->set("corp_access_token", $accessToken, time() + 7000); // corp access token有效期为7200秒，这里设置为7000秒
-#     }
-#
-#     public function getCorpAccessToken()
-#     {
-#         $memcache = $this->getMemcache();
-#         return $memcache->get("corp_access_token");
-#     }
-#
-#
-#
-#     private function getMemcache()
-#     {
-#         /*if (class_exists("Memcache"))
-#         {
-#             $memcache = new Memcache;
-#             if ($memcache->connect('localhost', 11211))
-#             {
-#                 return $memcache;
-#             }
-#         }*/
-#
-#         return new FileCache;
-#     }
-#
-#     public function get($key)
-#     {
-#         return $this->getMemcache()->get($key);
-#     }
-#
-#     public function set($key, $value)
-#     {
-#         $this->getMemcache()->set($key, $value);
-#     }
-# }
-#
-# /**
-#  * fallbacks
-#  */
-# class FileCache
-# {
-# 	function set($key, $value, $expire_time = 0) {
-#         if($key&&$value){
-#             $data = json_decode($this->get_file(DIR_ROOT ."filecache.php"),true);
-#             $item = array();
-#             $item["$key"] = $value;
-#
-#             $item['expire_time'] = $expire_time;
-#             $item['create_time'] = time();
-#             $data["$key"] = $item;
-#             $this->set_file("filecache.php",json_encode($data));
-#         }
-# 	}
-#
-# 	function get($key) {
-#         if($key){
-#             $data = json_decode($this->get_file(DIR_ROOT ."filecache.php"),true);
-#             if($data&&array_key_exists($key,$data)){
-#                 $item = $data["$key"];
-#                 if(!$item){
-#                     return false;
-#                 }
-#                 if($item['expire_time']>0&&$item['expire_time'] < time()){
-#                     return false;
-#                 }
-#
-#                 return $item["$key"];
-#             }else{
-#                 return false;
-#             }
-#
-#         }
-# 	}
-#
-#     function get_file($filename) {
-#         if (!file_exists($filename)) {
-#             $fp = fopen($filename, "w");
-#             fwrite($fp, "<?php exit();?>" . '');
-#             fclose($fp);
-#             return false;
-#         }else{
-#             $content = trim(substr(file_get_contents($filename), 15));
-#         }
-#         return $content;
-#     }
-#
-#     function set_file($filename, $content) {
-#         $fp = fopen($filename, "w");
-#         fwrite($fp, "<?php exit();?>" . $content);
-#         fclose($fp);
-#     }
-# }
+  def get(key)
+    if key
+      data = get_file(Rails.root.to_s + "/config/filecache.yml")
+      if data && data.has_key?(key)
+          item = data["#{key}"]
+          return false  if !item
+          if item['expire_time']>0 && item['expire_time'] < Time.now.to_i
+              return false;
+          end
+          return item["#{key}"]
+      else
+        return false;
+      end
+
+    end
+  end
+
+  def get_file(filename)
+    if !File.exist?(filename)
+      file = File.open(filename, "w")
+      file.write("")
+      file.close
+      return
+    else
+      content = YAML.load(File.read(filename))
+    end
+    return content
+  end
+
+  def set_file(filename, content)
+    # File.write(filename)
+    file = File.open(filename, "w")
+    file.write(content)
+    file.close unless file.nil?
+  end
+
+end
