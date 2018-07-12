c0b5ddadc690b114f67d4cc93cf5b1f6b8f428f3
liuchunliang <Chunliang.Liu@abcam.com>
2018-07-12 12:46:10 +0800

    部门更新完成

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,10 +1,5 @@
 class DepartmentsController < ApplicationController
-
-  def initialize
-    @auth = AuthService.new
-    @user = UserService.new
-    @departmentService = DepartmentService.new
-  end
+  before_action :init_locals, :only => :update_department
 
   def index
     @departments = Department.all
@@ -20,12 +15,23 @@ class DepartmentsController < ApplicationController
     if department_res["errmsg"] == "ok" && department_res["errcode"] == 0
       department_list = department_res["department"]
       department_list.each do |department|
-        Department.create(id: department["id"], name: department["name"], parentid: department["parentid"])
+        if Department.find(department["id"]).blank?
+          Department.create(id: department["id"], name: department["name"], parentid: department["parentid"])
+        else
+          Department.find(department["id"]).update(name: department["name"], parentid: department["parentid"])
+        end
+
       end
       return render plain: "共有部门#{department_list.count}个，更新个部门"
     else
       return render plain: department_res["errmsg"]
     end
+  end
+
+  def init_locals
+    @auth = AuthService.new
+    @user = UserService.new
+    @departmentService = DepartmentService.new
   end
 
 end
