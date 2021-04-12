b774c2137f4ea6d09bf2b9d9c5e8fdbe777c2ecb
clliu <chunliang.liu@abcam.com>
2021-04-12 10:33:20 +0800

    清理多余的adminlte文件

---
+++ app/assets/javascripts/golden_idea/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//    require_tree .
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+
+//= require golden_idea/jquery.min
+//= require golden_idea/jquery-ui.min
+//= require golden_idea/bootstrap.bundle.min
+//= require golden_idea/jquery.overlayScrollbars.min
+//= require golden_idea/Chart.min
+//= require golden_idea/bs-custom-file-input.min
+//= require golden_idea/select2.full.min
+//= require golden_idea/adminlte.min
+
+// 这个是简单富文本编辑器
+//= require wangEditor.min
+//= require wangEditor_fullscreen_plugin
+// 提示框
+//= require mTips
+// 图片点击变大
+//= require jquery.fancybox
+//
