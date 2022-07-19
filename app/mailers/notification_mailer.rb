3e8641183faa9487ef235b34ad3a4862efaa04cd
clliu <chunliang.liu@abcam.com>
2022-07-19 17:14:08 +0800

    金点子创建通知收件人

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,31 @@
 class NotificationMailer < ApplicationMailer
+
+  GoldenIdeaReceivers =  [
+    "jing.qian@abcam.com",
+    "chuanwei.lu@abcam.com",
+    "xin.zhang@abcam.com",
+    "yanli.chen@abcam.com",
+    "mengting.xu@abcam.com",
+    "xingchao.lu@abcam.com",
+    "sha.he@abcam.com",
+    "peng.zeng@abcam.com",
+    "tianqi.xing@abcam.com",
+    "yuqian.ying@abcam.com",
+    "jingjing.zhang@abcam.com",
+    "hui.wang@abcam.com",
+    "wei.zhao@abcam.com",
+    "xiong.fang@abcam.com",
+    "jungang.chen@abcam.com",
+    "yiting.zheng@abcam.com",
+    "chunliang.liu@abcam.com"
+  ]
+
   def proposal(title, content, real_name)
     @title = title
     @content = content
     @real_name = real_name
 
-    mail(to: ['chuanwei.lu@abcam.com', 'fan.ren@abcam.com', 'jing.qian@abcam.com', 'jingjing.xu@abcam.com', 'mengting.xu@abcam.com', 'peng.zeng@abcam.com', 'senliang.guo@abcam.com', 'sha.he@abcam.com', 'tianqi.xing@abcam.com', 'xin.zhang@abcam.com', 'xingchao.lu@abcam.com', 'yanli.chen@abcam.com', "xiong.fang@abcam.com", "chunliang.liu@abcam.com"], subject: "Proposal: #{@title}")
+    mail(to: GoldenIdeaReceivers, subject: "Proposal: #{@title}")
   end
 
   # 创建金点子之后给委员会发邮件
@@ -14,6 +35,6 @@ class NotificationMailer < ApplicationMailer
     @idea.attachments.each do |a|
       attachments[a.attachment_file_name] = File.read(Rails.root.join("public#{a.attachment.url}"))
     end
-    mail(to: ['chuanwei.lu@abcam.com', 'fan.ren@abcam.com', 'jing.qian@abcam.com', 'jingjing.xu@abcam.com', 'mengting.xu@abcam.com', 'peng.zeng@abcam.com', 'senliang.guo@abcam.com', 'sha.he@abcam.com', 'tianqi.xing@abcam.com', 'xin.zhang@abcam.com', 'xingchao.lu@abcam.com', 'yanli.chen@abcam.com', "xiong.fang@abcam.com", "chunliang.liu@abcam.com"], subject: "New Idea: #{@idea.title}")
+    mail(to: GoldenIdeaReceivers, subject: "New Idea: #{@idea.title}")
   end
 end
